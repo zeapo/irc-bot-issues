c003b45131c9a4280eb144c625abf4c800b89e6c
Mohamed Zenadi <mohamed@zenadi.com>
2014-11-24 22:23:56 +0100

    changed license to APLv2

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject irc-issues "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject irc-issues "0.0.1"
+  :description "IRC bot that handle issues auto-magically"
+  ;; :url "http://example.com/FIXME"
+  :license {:name "Apache License v2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot irc-issues.core
   :target-path "target/%s"
