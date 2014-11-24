e2f050afa9e752913e153043155480da4ce8a72d
Mohamed Zenadi <mohamed@zenadi.com>
2014-11-24 22:18:17 +0100

    Generated the project using Leiningen

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to irc-issues
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/irc_issues/core_test.clj
@@ -0,0 +1,7 @@
+(ns irc-issues.core-test
+  (:require [clojure.test :refer :all]
+            [irc-issues.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
