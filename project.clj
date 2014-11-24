09e70a5497c1865721d90401483815ef288fec8e
Mohamed Zenadi <mohamed@zenadi.com>
2014-11-24 23:26:06 +0100

    connect to irc and receives msgs

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   ;; :url "http://example.com/FIXME"
   :license {:name "Apache License v2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [irclj "0.5.0-alpha4"]]
   :main ^:skip-aot irc-issues.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/irc_issues/core.clj
+++ src/irc_issues/core.clj
@@ -1,7 +1,39 @@
 (ns irc-issues.core
+  (:require
+   [irclj.core :refer :all]
+   [irclj.parser :refer :all])
   (:gen-class))
 
+(def default-config
+  {
+   :server "irc.freenode.org"
+   :port 6667
+   :channel "#irc-issues-bot"
+   :nick "batbotbit"
+   })
+
+(defn callback [irc args]
+  "Called for each msg received"
+  (let [{text :text
+         target :target
+         nick :nick
+         host :host
+         command :command} args]
+    (println (str command "," text "," nick "," host))))
+
+;; Connect to the IRC server and join the channel
+(defn irc-bot
+  "The main entry point for the IRC bot"
+  [irc-config]
+  (def irc (connect
+            (:server irc-config)
+            (:port irc-config)
+            (:nick irc-config)
+            :callbacks {:privmsg callback}))
+  (join irc (:channel irc-config)))
+        
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "Launches the bot"
+  []
+  (irc-bot default-config))
